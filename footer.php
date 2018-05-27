af4814d0b13d30e52f91b2389e588259313a93b0
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2018-05-27 12:23:02 +0200

    Refactored header and footer

---
+++ footer.php
@@ -0,0 +1,80 @@
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-4 widget footer-widget">
+                <a class="footer-logo" href="index-2.html"><img src="images/logo-white.png" alt="Homely" /></a>
+                <p>Lorem ipsum dolor amet, consectetur adipiscing elit. Sed ut 
+                purus eget nunc ut dignissim cursus at a nisl. Mauris vitae 
+                turpis quis eros egestas tempor sit amet a arcu. Duis egestas 
+                hendrerit diam.</p>
+                <div class="divider"></div>
+                <ul class="social-icons circle">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 widget footer-widget from-the-blog">
+                <h4><span>From the Blog</span> <img src="images/divider-half.png" alt="" /></h4>
+                <ul>
+                    <li>
+                      <a href="#"><h3>Open House at 123 Smith Drive</h3></a>
+                      <p>Vel fermentum ipsum. Quis molestie odio. Interdum et...<br/> <a href="#">Read More</a></p>
+                      <div class="clear"></div>
+                    </li>
+                     <li>
+                        <a href="#"><h3>Open House at 123 Smith Drive</h3></a>
+                        <p>Vel fermentum ipsum. Quis molestie odio. Interdum et...<br/> <a href="#">Read More</a></p>
+                        <div class="clear"></div>
+                      </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 widget footer-widget">
+                <h4><span>Get In Touch</span> <img src="images/divider-half.png" alt="" /></h4>
+                <p>123 Smith Drive<br/>
+                Annapolis, MD 21012<br/>
+                United States
+                </p>
+                <p>
+                <b class="open-hours">Open Hours</b><br/>
+                Mondy - Friday: 9 am - 5 pm<br/>
+                Saturday: 9 am - 1pm<br/>
+                Sunday: Closed
+                </p>
+                <p class="footer-phone"><i class="fa fa-phone icon"></i> (123) 456-7890</p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 widget footer-widget newsletter">
+                <h4><span>Newsletter</span> <img src="images/divider-half.png" alt="" /></h4>
+                <p><b>Subscribe to our newsletter!</b> Vel lorem ipsum. Lorem molestie odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
+                <form class="subscribe-form" method="post" action="#">
+                    <input type="text" name="email" value="Your email" />
+                    <input type="submit" name="submit" value="SEND" class="button small alt" />
+                </form>
+            </div>
+        </div><!-- end row -->
+    </div><!-- end footer container -->
+</footer>
+
+<div class="bottom-bar">
+    <div class="container">
+    © 2017  |  Homely - A theme by <a href="http://rypecreative.com/" target="_blank">Rype Creative</a>  |  All Rights Reserved
+    </div>
+</div>
+
+<!-- JavaScript file links -->
+<script src="js/jquery-3.1.1.min.js"></script>      <!-- Jquery -->
+<script src="assets/jquery-ui-1.12.1/jquery-ui.min.js"></script>
+<script src="js/bootstrap.min.js"></script>  <!-- bootstrap 3.0 -->
+<script src="assets/slick-1.6.0/slick.min.js"></script> <!-- slick slider -->
+<script src="assets/chosen-1.6.2/chosen.jquery.min.js"></script> <!-- chosen - for select dropdowns -->
+<script src="js/isotope.min.js"></script> <!-- isotope-->
+<script src="js/wNumb.js"></script> <!-- price formatting -->
+<script src="js/nouislider.min.js"></script> <!-- price slider -->
+<script src="js/global.js"></script>
+
+
+</body>
+</html>
