40113c07cd6c06cfbf422103300f34b8099032b6
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2018-05-27 13:50:34 +0200

    Added functionality to dashbaord

--- dashboard.php
+++ dashboard.php
@@ -1,163 +1,114 @@
+<?php
+
+include 'functions.php';
+
+/*if(isset($_SESSION['user_id']) == '' ) {
+	header("location:login.php");
+}*/
+
+
+
+
+?>
+
 <?php 
 include 'header.php';
 ?>
 
-
 <section class="subheader">
-  <div class="container">
-    <h1>My Properties</h1>
-    <div class="breadcrumb right">Home <i class="fa fa-angle-right"></i> <a href="#" class="current">My Properties</a></div>
-    <div class="clear"></div>
-  </div>
+	<div class="container">
+		<h1>My Properties</h1>
+		<div class="breadcrumb right">Home <i class="fa fa-angle-right"></i> <a href="#" class="current">My Properties</a></div>
+		<div class="clear"></div>
+	</div>
 </section>
 
 <section class="module my-properties">
-  <div class="container">
-  
-	<div class="row">
-		<div class="col-lg-3 col-md-3 sidebar-left">
-			<div class="widget member-card">
-				<div class="member-card-header">
-					<a href="#" class="member-card-avatar"><img src="images/agent-img3.jpg" alt="" /></a>
-					<h3>John Doe</h3>
-					<p><i class="fa fa-envelope icon"></i>jdoe@gmail.com</p>
+	<div class="container">
+
+		<div class="row">
+			<?php include 'links.php'; ?>;
+			<div class="col-lg-9 col-md-9">
+				<?php
+
+				$sql = "SELECT * FROM tbl_prop WHERE ref='".$_SESSION['email']."'";
+				$res = mysqli_query($con, $sql);
+
+				if (mysqli_num_rows($res) > 0) {
+					echo '
+					<table class="my-properties-list">
+					<tr>
+					<th>Image</th>
+					<th>Property</th>
+					<th>Post Status</th>
+					<th>Date Created</th>
+					<th>Actions</th>
+					</tr>';
+					while ($row = mysqli_fetch_assoc($res)) {
+
+
+						echo '
+						<tr>
+						<td class="property-img"><a href="property-single.html"><img src="images/property-img4.jpg" alt="" /></a>'.$row['id'].'</td>
+						<td class="property-title">
+						<a href="property-single.html">Modern Family Home</a><br/>
+						<p class="property-address"><i class="fa fa-map-marker icon"></i>123 Smith Drive, Baltimore, MD</p>
+						<p><strong>$253,000</strong></p>'.$row['name'].' '.$row['surname'].'</td>
+						<td class="property-post-status"><span class="button small alt">Published</span>'.$row['email'].'</td>
+						<td class="property-actions">
+						<a href="#"><i class="fa fa-eye icon"></i>View</a>
+						<a href="#"><i class="fa fa-pencil icon"></i>Edit</a>
+						<a href="#"><i class="fa fa-close icon"></i>Delete</a>
+						</td>
+						</tr>';
+						}echo '
+						</table>
+
+						<div class="pagination">
+						<div class="center">
+						<ul>
+						<li><a href="#" class="button small grey"><i class="fa fa-angle-left"></i></a></li>
+						<li class="current"><a href="#" class="button small grey">1</a></li>
+						<li><a href="#" class="button small grey">2</a></li>
+						<li><a href="#" class="button small grey">3</a></li>
+						<li><a href="#" class="button small grey"><i class="fa fa-angle-right"></i></a></li>
+						</ul>
+						</div>
+						<div class="clear"></div>
+						</div>';
+					} else {
+						echo '<div class="alert alert-warning">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<strong>No Properties found.</strong>
+						</div>';
+					}
+
+					?>
+				</div><!-- end col -->
+			</div><!-- end row -->
+
+		</div><!-- end container -->
+	</section>
+
+	<section class="module cta newsletter">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 col-md-7">
+					<h3>Sign up for our <strong>newsletter.</strong></h3>
+					<p>Lorem molestie odio. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
 				</div>
-				<div class="member-card-content">
-					<img class="hex" src="images/hexagon.png" alt="" />
-					<ul>
-						<li><a href="user-profile.html"><i class="fa fa-user icon"></i>Profile</a></li>
-						<li class="active"><a href="user-my-properties.html"><i class="fa fa-home icon"></i>My Properties</a></li>
-						<li><a href="user-favorite-properties.html"><i class="fa fa-heart icon"></i>Favorited Properties</a></li>
-						<li><a href="user-submit-property.html"><i class="fa fa-plus icon"></i>Submit Property</a></li>
-						<li><a href="#"><i class="fa fa-reply icon"></i>Logout</a></li>
-					</ul>
+				<div class="col-lg-5 col-md-5">
+					<form method="post" id="newsletter-form" class="newsletter-form">
+						<input type="email" placeholder="Your email..." />
+						<button type="submit" form="newsletter-form"><i class="fa fa-send"></i></button>
+					</form>
 				</div>
-			</div>
-		</div>
-
-		<div class="col-lg-9 col-md-9">
-			<table class="my-properties-list">
-			  <tr>
-				<th>Image</th>
-				<th>Property</th>
-				<th>Post Status</th>
-				<th>Date Created</th>
-				<th>Actions</th>
-			  </tr>
-			  <tr>
-				<td class="property-img"><a href="property-single.html"><img src="images/property-img4.jpg" alt="" /></a></td>
-				<td class="property-title">
-					<a href="property-single.html">Modern Family Home</a><br/>
-					<p class="property-address"><i class="fa fa-map-marker icon"></i>123 Smith Drive, Baltimore, MD</p>
-					<p><strong>$253,000</strong></p>
-				</td>
-				<td class="property-post-status"><span class="button small alt">Published</span></td>
-				<td class="property-date">2/27/2017</td>
-				<td class="property-actions">
-					<a href="#"><i class="fa fa-eye icon"></i>View</a>
-					<a href="#"><i class="fa fa-pencil icon"></i>Edit</a>
-					<a href="#"><i class="fa fa-close icon"></i>Delete</a>
-				</td>
-			  </tr>
-			  <tr>
-				<td class="property-img"><a href="property-single.html"><img src="images/property-img1.jpg" alt="" /></a></td>
-				<td class="property-title">
-					<a href="property-single.html">Ubran Apartment</a><br/>
-					<p class="property-address"><i class="fa fa-map-marker icon"></i>123 Smith Drive, Baltimore, MD</p>
-					<p><strong>$12,000</strong> Per Month</p>
-				</td>
-				<td class="property-post-status"><span class="button small grey">Pending</span></td>
-				<td class="property-date">2/27/2017</td>
-				<td class="property-actions">
-					<a href="#"><i class="fa fa-eye icon"></i>View</a>
-					<a href="#"><i class="fa fa-pencil icon"></i>Edit</a>
-					<a href="#"><i class="fa fa-close icon"></i>Delete</a>
-				</td>
-			  </tr>
-			  <tr>
-				<td class="property-img"><a href="property-single.html"><img src="images/property-img2.jpg" alt="" /></a></td>
-				<td class="property-title">
-					<a href="property-single.html">Modern Family Home</a><br/>
-					<p class="property-address"><i class="fa fa-map-marker icon"></i>123 Smith Drive, Baltimore, MD</p>
-					<p><strong>$253,000</strong></p>
-				</td>
-				<td class="property-post-status"><span class="button small alt">Published</span></td>
-				<td class="property-date">2/27/2017</td>
-				<td class="property-actions">
-					<a href="#"><i class="fa fa-eye icon"></i>View</a>
-					<a href="#"><i class="fa fa-pencil icon"></i>Edit</a>
-					<a href="#"><i class="fa fa-close icon"></i>Delete</a>
-				</td>
-			  </tr>
-			  <tr>
-				<td class="property-img"><a href="property-single.html"><img src="images/property-img3.jpg" alt="" /></a></td>
-				<td class="property-title">
-					<a href="property-single.html">Modern Family Home</a><br/>
-					<p class="property-address"><i class="fa fa-map-marker icon"></i>123 Smith Drive, Baltimore, MD</p>
-					<p><strong>$253,000</strong></p>
-				</td>
-				<td class="property-post-status"><span class="button small alt">Published</span></td>
-				<td class="property-date">2/27/2017</td>
-				<td class="property-actions">
-					<a href="#"><i class="fa fa-eye icon"></i>View</a>
-					<a href="#"><i class="fa fa-pencil icon"></i>Edit</a>
-					<a href="#"><i class="fa fa-close icon"></i>Delete</a>
-				</td>
-			  </tr>
-			  <tr>
-				<td class="property-img"><a href="property-single.html"><img src="images/property-img4.jpg" alt="" /></a></td>
-				<td class="property-title">
-					<a href="property-single.html">Modern Family Home</a><br/>
-					<p class="property-address"><i class="fa fa-map-marker icon"></i>123 Smith Drive, Baltimore, MD</p>
-					<p><strong>$253,000</strong></p>
-				</td>
-				<td class="property-post-status"><span class="button small alt">Published</span></td>
-				<td class="property-date">2/27/2017</td>
-				<td class="property-actions">
-					<a href="#"><i class="fa fa-eye icon"></i>View</a>
-					<a href="#"><i class="fa fa-pencil icon"></i>Edit</a>
-					<a href="#"><i class="fa fa-close icon"></i>Delete</a>
-				</td>
-			  </tr>
-			</table>
-			
-			<div class="pagination">
-				<div class="center">
-					<ul>
-					  <li><a href="#" class="button small grey"><i class="fa fa-angle-left"></i></a></li>
-					  <li class="current"><a href="#" class="button small grey">1</a></li>
-					  <li><a href="#" class="button small grey">2</a></li>
-					  <li><a href="#" class="button small grey">3</a></li>
-					  <li><a href="#" class="button small grey"><i class="fa fa-angle-right"></i></a></li>
-					</ul>
-				</div>
-				<div class="clear"></div>
-			</div>
-		</div><!-- end col -->
-	</div><!-- end row -->
-
-  </div><!-- end container -->
-</section>
-
-<section class="module cta newsletter">
-  <div class="container">
-	<div class="row">
-		<div class="col-lg-7 col-md-7">
-			<h3>Sign up for our <strong>newsletter.</strong></h3>
-			<p>Lorem molestie odio. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-		</div>
-		<div class="col-lg-5 col-md-5">
-			<form method="post" id="newsletter-form" class="newsletter-form">
-				<input type="email" placeholder="Your email..." />
-				<button type="submit" form="newsletter-form"><i class="fa fa-send"></i></button>
-			</form>
-		</div>
-	</div><!-- end row -->
-  </div><!-- end container -->
-</section>
+			</div><!-- end row -->
+		</div><!-- end container -->
+	</section>
 
 
 
-<?php 
-include 'footer.php';
-?>
+	<?php 
+	include 'footer.php';
+	?>
