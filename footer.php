9313f5c70289832c9e9fa3f6386edc0e46b25dad
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2018-05-28 11:53:53 +0200

    Added datatables plugin

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
             <div class="col-lg-3 col-md-3 col-sm-4 widget footer-widget">
                 <a class="footer-logo" href="index-2.html"><img src="images/logo-white.png" alt="Homely" /></a>
                 <p>Lorem ipsum dolor amet, consectetur adipiscing elit. Sed ut 
-                purus eget nunc ut dignissim cursus at a nisl. Mauris vitae 
-                turpis quis eros egestas tempor sit amet a arcu. Duis egestas 
+                    purus eget nunc ut dignissim cursus at a nisl. Mauris vitae 
+                    turpis quis eros egestas tempor sit amet a arcu. Duis egestas 
                 hendrerit diam.</p>
                 <div class="divider"></div>
                 <ul class="social-icons circle">
@@ -24,43 +24,43 @@
                       <a href="#"><h3>Open House at 123 Smith Drive</h3></a>
                       <p>Vel fermentum ipsum. Quis molestie odio. Interdum et...<br/> <a href="#">Read More</a></p>
                       <div class="clear"></div>
-                    </li>
-                     <li>
-                        <a href="#"><h3>Open House at 123 Smith Drive</h3></a>
-                        <p>Vel fermentum ipsum. Quis molestie odio. Interdum et...<br/> <a href="#">Read More</a></p>
-                        <div class="clear"></div>
-                      </li>
-                </ul>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-4 widget footer-widget">
-                <h4><span>Get In Touch</span> <img src="images/divider-half.png" alt="" /></h4>
-                <p>123 Smith Drive<br/>
+                  </li>
+                  <li>
+                    <a href="#"><h3>Open House at 123 Smith Drive</h3></a>
+                    <p>Vel fermentum ipsum. Quis molestie odio. Interdum et...<br/> <a href="#">Read More</a></p>
+                    <div class="clear"></div>
+                </li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-4 widget footer-widget">
+            <h4><span>Get In Touch</span> <img src="images/divider-half.png" alt="" /></h4>
+            <p>123 Smith Drive<br/>
                 Annapolis, MD 21012<br/>
                 United States
-                </p>
-                <p>
+            </p>
+            <p>
                 <b class="open-hours">Open Hours</b><br/>
                 Mondy - Friday: 9 am - 5 pm<br/>
                 Saturday: 9 am - 1pm<br/>
                 Sunday: Closed
-                </p>
-                <p class="footer-phone"><i class="fa fa-phone icon"></i> (123) 456-7890</p>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-12 widget footer-widget newsletter">
-                <h4><span>Newsletter</span> <img src="images/divider-half.png" alt="" /></h4>
-                <p><b>Subscribe to our newsletter!</b> Vel lorem ipsum. Lorem molestie odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
-                <form class="subscribe-form" method="post" action="#">
-                    <input type="text" name="email" value="Your email" />
-                    <input type="submit" name="submit" value="SEND" class="button small alt" />
-                </form>
-            </div>
-        </div><!-- end row -->
-    </div><!-- end footer container -->
+            </p>
+            <p class="footer-phone"><i class="fa fa-phone icon"></i> (123) 456-7890</p>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 widget footer-widget newsletter">
+            <h4><span>Newsletter</span> <img src="images/divider-half.png" alt="" /></h4>
+            <p><b>Subscribe to our newsletter!</b> Vel lorem ipsum. Lorem molestie odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
+            <form class="subscribe-form" method="post" action="#">
+                <input type="text" name="email" value="Your email" />
+                <input type="submit" name="submit" value="SEND" class="button small alt" />
+            </form>
+        </div>
+    </div><!-- end row -->
+</div><!-- end footer container -->
 </footer>
 
 <div class="bottom-bar">
     <div class="container">
-    © 2017  |  Homely - A theme by <a href="http://rypecreative.com/" target="_blank">Rype Creative</a>  |  All Rights Reserved
+        © 2017  |  Homely - A theme by <a href="http://rypecreative.com/" target="_blank">Rype Creative</a>  |  All Rights Reserved
     </div>
 </div>
 
@@ -74,6 +74,8 @@
 <script src="js/wNumb.js"></script> <!-- price formatting -->
 <script src="js/nouislider.min.js"></script> <!-- price slider -->
 <script src="js/global.js"></script>
+<script src="assets/datatables/js/dataTables.bootstrap.min.js"></script>
+<script src="assets/datatables/js/jquery.dataTables.min.js"></script>
 
 
 </body>
--- header.php
+++ header.php
@@ -17,6 +17,7 @@
   <link href="css/nouislider.min.css" rel="stylesheet">
   <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
   <link href="css/responsive.css" rel="stylesheet" type="text/css" media="all" />
+  <link href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
